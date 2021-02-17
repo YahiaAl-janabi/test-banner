fe8042150c6409bcc74e53c69362b24285f4d371
Yahia Al-janabi <yahiaaljanabi@gmail.com>
2021-02-17 22:07:12 +0000

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+var option1 = document.getElementById("option1");
+var option2 = document.getElementById("option2");
+var container1 = document.getElementById("container");
+var container2 = document.getElementById("container2");
+var container3 = document.getElementById("container3");
+var body = document.querySelector("body");
+
+function display2() {
+  container2.style.display = "block";
+  container1.style.display = "none";
+  container3.style.display = "none";
+}
+
+function display3() {
+  container2.style.display = "none";
+  container1.style.display = "none";
+  container3.style.display = "block";
+}
+
+option1.onclick = function () {
+  display2();
+};
+
+option2.onclick = function () {
+  display3();
+};
